7151006daa51161ec39b3a51916e4d3158dbbd8d
Miguel Guilherme <mguilherme82@gmail.com>
2017-03-12 00:30:52 +0000

    Refactor

---
+++ lab3/lab-3-client/src/main/java/com/guilherme/miguel/controller/LuckyWordController.java
@@ -0,0 +1,23 @@
+package com.guilherme.miguel.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static java.lang.String.format;
+
+/**
+ * @author Miguel Guilherme
+ */
+@RestController
+public class LuckyWordController {
+
+    @Value("${lucky-word}")
+    String luckyWord;
+
+    @RequestMapping("/lucky-word")
+    public String showLuckyWord() {
+        return format("The lucky word is: {}", luckyWord);
+    }
+
+}
