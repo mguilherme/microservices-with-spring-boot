d87c306a54985120044ce9707c957760db16b4bc
Miguel Guilherme <mguilherme82@gmail.com>
2017-03-12 21:42:50 +0000

    Lab 5 Refactor

--- lab5/eureka-client/src/main/java/demo/SentenceController.java
+++ lab5/eureka-client/src/main/java/demo/SentenceController.java
@@ -1,42 +1,43 @@
 package demo;
 
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
 import java.net.URI;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.client.RestTemplate;
 
-
-
-@Controller
+@RestController
 public class SentenceController {
 
-	@Autowired DiscoveryClient client;
-	
-	@RequestMapping("/sentence")
-	public @ResponseBody String getSentence() {
-	  return String.format("%s %s %s %s %s.",
-		  getWord("LAB-4-SUBJECT"),
-		  getWord("LAB-4-VERB"),
-		  getWord("LAB-4-ARTICLE"),
-		  getWord("LAB-4-ADJECTIVE"),
-		  getWord("LAB-4-NOUN") );
-	}
+    private DiscoveryClient client;
 
-	public String getWord(String service) {
+    public SentenceController(DiscoveryClient client) {
+        this.client = client;
+    }
+
+    @GetMapping("/sentence")
+    public String getSentence() {
+        return String.format("%s %s %s %s %s.",
+                getWord("LAB-5-SUBJECT"),
+                getWord("LAB-5-VERB"),
+                getWord("LAB-5-ARTICLE"),
+                getWord("LAB-5-ADJECTIVE"),
+                getWord("LAB-5-NOUN"));
+    }
+
+    public String getWord(String service) {
         List<ServiceInstance> list = client.getInstances(service);
-        if (list != null && list.size() > 0 ) {
-      	URI uri = list.get(0).getUri();
-	      	if (uri !=null ) {
-	      		return (new RestTemplate()).getForObject(uri,String.class);
-	      	}
+        if (list != null && list.size() > 0) {
+            URI uri = list.get(0).getUri();
+            if (uri != null) {
+                return (new RestTemplate()).getForObject(uri, String.class);
+            }
         }
         return null;
-	}
+    }
 
 }
