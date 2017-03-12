b8a1330468f1f9077f25c0ec785e3aa485f79032
Miguel Guilherme <mguilherme82@gmail.com>
2017-03-12 22:20:53 +0000

    Add Ribbon Load Balancer

--- lab5/eureka-client/src/main/java/demo/SentenceController.java
+++ lab5/eureka-client/src/main/java/demo/SentenceController.java
@@ -1,27 +1,26 @@
 package demo;
 
 import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.URI;
-import java.util.List;
+import static java.lang.String.format;
 
 
 @RestController
 public class SentenceController {
 
-    private DiscoveryClient client;
+    private LoadBalancerClient client;
 
-    public SentenceController(DiscoveryClient client) {
+    public SentenceController(LoadBalancerClient client) {
         this.client = client;
     }
 
     @GetMapping("/sentence")
     public String getSentence() {
-        return String.format("%s %s %s %s %s.",
+        return format("%s %s %s %s %s.",
                 getWord("LAB-5-SUBJECT"),
                 getWord("LAB-5-VERB"),
                 getWord("LAB-5-ARTICLE"),
@@ -30,12 +29,9 @@ public class SentenceController {
     }
 
     public String getWord(String service) {
-        List<ServiceInstance> list = client.getInstances(service);
-        if (list != null && list.size() > 0) {
-            URI uri = list.get(0).getUri();
-            if (uri != null) {
-                return (new RestTemplate()).getForObject(uri, String.class);
-            }
+        ServiceInstance serviceInstance = client.choose(service);
+        if (serviceInstance != null) {
+            return (new RestTemplate()).getForObject(serviceInstance.getUri(), String.class);
         }
         return null;
     }
