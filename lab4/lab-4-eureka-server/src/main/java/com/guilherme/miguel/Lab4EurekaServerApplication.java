9e3ba4ddd7d45fb871ad610200558e9cf3b339ae
Miguel Guilherme <mguilherme82@gmail.com>
2017-03-12 02:29:30 +0000

    Add eureka server to lab 4

---
+++ lab4/lab-4-eureka-server/src/main/java/com/guilherme/miguel/Lab4EurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.guilherme.miguel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class Lab4EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab4EurekaServerApplication.class, args);
+    }
+}
